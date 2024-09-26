c35c5a9f54eb8687f077948770681193ee23722b
abhishekanand22 <m.abhishekanand9@gmail.com>
2024-09-26 18:23:07 +0530

    ToDo App : HTML, CSS, JS

---
+++ todo-app/app.js
@@ -0,0 +1,8 @@
+let btn = document.querySelector(".addButton");
+let ul = document.querySelector(".unList");
+let inp = document.querySelector("input");
+
+
+btn.addEventListener("click", function() {
+    
+})
