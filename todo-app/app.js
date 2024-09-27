f09cf3cdc6ad93b5d442eab31b3d8d33f56007d5
abhishekanand22 <m.abhishekanand9@gmail.com>
2024-09-27 14:45:46 +0530

    ToDo App : HTML, CSS, JS

--- todo-app/app.js
+++ todo-app/app.js
@@ -4,5 +4,26 @@ let inp = document.querySelector("input");
 
 
 btn.addEventListener("click", function() {
+    let item = document.createElement("li");
+    item.innerText = inp.value;
+
+    let dltBtn = document.createElement("button");
+    dltBtn.innerText = "Mark as Done";
+    dltBtn.classList.add("delete");
     
-})
+
+    item.appendChild(dltBtn);
+    ul.appendChild(item);
+    inp.value="";
+});
+
+
+
+ul.addEventListener("click" , function(event){
+     if(event.target.nodeName="BUTTON")
+     {
+        let listItem = event.target.parentElement;
+        listItem.remove();
+     }
+});
+
